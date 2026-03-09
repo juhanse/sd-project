eca4af2d97fd3e3cdc71e778b5bc0838db441c4c
Julien Hanse <julien.hanse@student.vinci.be>
2026-03-09 11:21:17 +0100

    fix: altitude attribut

--- Localisation.java
+++ Localisation.java
@@ -1,36 +1,36 @@
 public class Localisation {
     private String id;
-    private double altitude;
-    private double longitude;
     private String nom;
-    private double metres;
+    private double latitude;
+    private double longitude;
+    private double altitude;
 
-    public Localisation(String id, double altitude, double longitude, String nom, double metres) {
+    public Localisation(String id, String nom, double latitude, double longitude, double altitude) {
         this.id = id;
-        this.altitude = altitude;
-        this.longitude = longitude;
         this.nom = nom;
-        this.metres = metres;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.altitude = altitude;
     }
 
     public String getId() {
         return this.id;
     }
 
-    public double getAltitude() {
-        return this.altitude;
+    public String getNom() {
+        return nom;
+    }
+
+    public double getlatitude() {
+        return this.latitude;
     }
 
     public double getLongitude() {
         return longitude;
     }
 
-    public String getNom() {
-        return nom;
-    }
-
-    public double getMetres() {
-        return metres;
+    public double getAltitude() {
+        return altitude;
     }
 
     public String toString() {
